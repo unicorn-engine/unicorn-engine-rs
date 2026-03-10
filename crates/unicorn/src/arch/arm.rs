6542464d72bc587ad56a16dbd04b40a4b59ed40c
funsafemath@proton.me <funsafemath@proton.me>
2026-03-10 22:43:27 +0000

    Move coprocessor functions to Arm and Arm64 modules, make them type-safe

--- crates/unicorn/src/arch/arm.rs
+++ crates/unicorn/src/arch/arm.rs
@@ -1,8 +1,39 @@
-use unicorn_engine_sys::{Mode, RegisterARM, uc_error};
+use unicorn_engine_sys::{Mode, RegisterARM, RegisterARMCP, uc_error, uc_reg_read, uc_reg_write};
 
-use crate::arch::{Register, UcArch};
+use crate::{
+    Unicorn,
+    arch::{Register, UcArch},
+};
 
 pub enum Arm {}
 
 impl_arch!(Arm, RegisterARM, unicorn_engine_sys::Arch::ARM);
 impl_reg_pc_counter!(RegisterARM);
+
+// todo: find out if coprocessor functions can fail if the arch is correct
+// if they are infallible, Result is not needed
+impl<D> Unicorn<'_, D, Arm> {
+    /// Read ARM Coprocessor register
+    pub fn reg_read_arm_coproc(&self, reg: &mut RegisterARMCP) -> Result<(), uc_error> {
+        unsafe {
+            uc_reg_read(
+                self.get_handle(),
+                RegisterARM::CP_REG.into(),
+                core::ptr::from_mut(reg).cast(),
+            )
+        }
+        .into()
+    }
+
+    /// Write ARM Coprocessor register
+    pub fn reg_write_arm_coproc(&mut self, reg: &RegisterARMCP) -> Result<(), uc_error> {
+        unsafe {
+            uc_reg_write(
+                self.get_handle(),
+                RegisterARM::CP_REG.into(),
+                core::ptr::from_ref(reg).cast(),
+            )
+        }
+        .into()
+    }
+}
--- crates/unicorn/src/arch/arm64.rs
+++ crates/unicorn/src/arch/arm64.rs
@@ -1,8 +1,41 @@
-use unicorn_engine_sys::{Mode, RegisterARM64, uc_error};
+use unicorn_engine_sys::{
+    Mode, RegisterARM64, RegisterARM64CP, uc_error, uc_reg_read, uc_reg_write,
+};
 
-use crate::arch::{Register, UcArch};
+use crate::{
+    Unicorn,
+    arch::{Register, UcArch},
+};
 
 pub enum Arm64 {}
 
 impl_arch!(Arm64, RegisterARM64, unicorn_engine_sys::Arch::ARM64);
 impl_reg_pc_counter!(RegisterARM64);
+
+// todo: find out if coprocessor functions can fail if the arch is correct
+// if they are infallible, Result is not needed
+impl<D> Unicorn<'_, D, Arm64> {
+    /// Read ARM64 Coprocessor register
+    pub fn reg_read_arm64_coproc(&self, reg: &mut RegisterARM64CP) -> Result<(), uc_error> {
+        unsafe {
+            uc_reg_read(
+                self.get_handle(),
+                RegisterARM64::CP_REG.into(),
+                core::ptr::from_mut(reg).cast(),
+            )
+        }
+        .and(Ok(()))
+    }
+
+    /// Write ARM64 Coprocessor register
+    pub fn reg_write_arm64_coproc(&mut self, reg: &RegisterARM64CP) -> Result<(), uc_error> {
+        unsafe {
+            uc_reg_write(
+                self.get_handle(),
+                RegisterARM64::CP_REG.into(),
+                core::ptr::from_ref(reg).cast(),
+            )
+        }
+        .and(Ok(()))
+    }
+}
